5d911ca76f5164d723e9f5a047242196c1ef34e7
adas12 <arijit_das@intuit.com>
2024-04-22 15:02:10 +0530

    css

--- src/UI/Shared/Shared.tsx
+++ src/UI/Shared/Shared.tsx
@@ -8,12 +8,13 @@ interface ContentWrapperProps {
 
 export const ContentWrapper = (props: ContentWrapperProps) => {
   const { children, header } = props;
+
   return (
     <div className={styles.contentWrapperContainer}>
       <h3 className={classNames([styles.headerName, styles.removeMargin])}>
         {header}
       </h3>
-      <div className={styles.contentWrapper}>{children}</div>;
+      <div className={styles.contentWrapper}>{children}</div>
     </div>
   );
 };
